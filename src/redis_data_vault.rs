86c97eac935c78a237c4bda976701424826b90d0
tcross <tom.bz2@gmail.com>
2020-08-05 16:22:04 -0500

    refactor store/retrieve to make modular/reusable methods

--- src/redis_data_vault.rs
+++ src/redis_data_vault.rs
@@ -1,7 +1,7 @@
 use async_trait::async_trait;
 use credit_card::CreditCard;
 use deadpool_redis::redis::AsyncCommands;
-use crate::data_vault::{DataVault};
+use crate::traits::{DataVault};
 use crate::config::DeadpoolRedisConfig;
 use crate::encryption::Aes128CbcEncryption;
 use crate::tokenizer::{Tokenizer, Blake3Tokenizer};
@@ -43,24 +43,47 @@ impl DataVault for RedisDataVault {
         }
     }
 
-    /// Store the credit card with the given token as the redis key
-    /// * `token` - the token that maps to this credit card data (PAN)
-    /// * `credit_card` - the cc object that you wish to store
-    async fn store(&self, credit_card: &CreditCard) -> String {
-        let token = self.tokenizer.generate(&credit_card);
-
+    /// Encrypt and Store a string with the given token as the redis key
+    /// Arguments:
+    ///     * `CreditCard` - the cc object that you wish to store
+    /// return:
+    ///     the token as String
+    async fn store(&self, token: &String, string: &String) {
         let mut conn = self.pool.get().await.unwrap();
+        let encrypted_json = self.encryption.encrypt(string.as_bytes());
+        let _:() = conn.set(token, encrypted_json).await.unwrap_or_default();
+    }
+
+    /// Store the credit card in the data vault
+    /// Arguments:
+    ///     * `CreditCard` - the cc object that you wish to store
+    /// return:
+    ///     A new token as String
+    async fn store_credit_card(&self, credit_card: &CreditCard) -> String {
+        let token = self.tokenizer.generate(&credit_card);
         let credit_card_json = serde_json::to_string(&credit_card).unwrap();
-        let encrypted_json = self.encryption.encrypt(credit_card_json.as_bytes());
-        let _:() = conn.set(&token, encrypted_json).await.unwrap_or_default();
+        self.store(&token, &credit_card_json).await;
         token
     }
 
-    /// Get the credit card from the data vault given token
-    async fn retrieve(&self, token: &String) -> CreditCard {
+    /// Get decrypted arbitrary data from the vault by token
+    /// Arguments:
+    ///     * `token`: the string form of the ID of the data
+    /// returns:
+    ///     * the decrypted string of data
+    async fn retrieve(&self, token: &String) -> String {
         let mut conn = self.pool.get().await.unwrap();
         let encrypted_credit_card_json: Vec<u8> = conn.get(token).await.unwrap_or_default();
-        let credit_card_json = self.encryption.decrypt(encrypted_credit_card_json.as_slice());
+        self.encryption.decrypt(encrypted_credit_card_json.as_slice())
+    }
+
+    /// Get the credit card from the data vault given a token
+    /// Arguments:
+    ///     * `token`: the string form of the ID of the data
+    /// returns:
+    ///     * `CreditCard` object
+    async fn retrieve_credit_card(&self, token: &String) -> CreditCard {
+        let credit_card_json = self.retrieve(token).await;
         serde_json::from_str(&credit_card_json).unwrap_or_default()
     }
 }
