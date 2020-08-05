d7580f6ce9963c9cd89e96729391f0f152b0850b
tcross <tom.bz2@gmail.com>
2020-08-05 16:20:24 -0500

    move DataVault trait to a new traits.rs (ambiguous name (same as this package)

---
+++ src/traits.rs
@@ -0,0 +1,14 @@
+use async_trait::async_trait;
+use credit_card::CreditCard;
+
+/// This is what a Data Vault can do
+/// It's fundamental purpose is to store and retrieve
+/// data in a secure encrypted manner
+#[async_trait]
+pub trait DataVault {
+    fn new() -> Self;
+    async fn store(&self, token: &String, string: &String);
+    async fn store_credit_card(&self, credit_card: &CreditCard) -> String;
+    async fn retrieve(&self, token: &String) -> String;
+    async fn retrieve_credit_card(&self, token: &String) -> CreditCard;
+}
