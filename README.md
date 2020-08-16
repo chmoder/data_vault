ccd22b79f6a02f64d2d65b2fd02f0efb4fac643c
tcross <tom.bz2@gmail.com>
2020-08-15 22:40:54 -0500

    doc - update cargo and github

--- README.md
+++ README.md
@@ -10,40 +10,53 @@ Data Vault is a library for storing and retrieving Credit Card data via Tokens.
 [![License](https://img.shields.io/crates/l/data_vault.svg)](https://img.shields.io/crates/l/data_vault.svg)
 
 
-#### add data_vault as a dependency to Cargo.toml 
 ```toml
+# Cargo.toml
+[dependencies]
 data_vault = "^0.2"
 ```
 
-```rust,norun
-# traits
-use crate::traits::DataVault;
-use crate::encryption::traits::Encryption;
+```dotenv
+# .env OR environment variables
+REDIS_URL=redis://:foobared@127.0.0.1/
+# REDIS_POOL_MAX_SIZE=16
+ENCRYPTED_DATA_VAULT_KEY=000102030405060708090a0b0c0d0e0f
+ENCRYPTED_DATA_VAULT_IV=f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff
+```
 
-# data vault
-use crate::redis_data_vault::RedisDataVault;
-# swappable encryption
-use crate::encryption::AesGcmSivEncryption;
-# swappable tokenizer
-use crate::tokenizer::Blake3Tokenizer;
+```rust
+// example.rs
 
-# credit card type
+// traits
+use data_vault::DataVault;
+use data_vault::encryption::traits::Encryption;
+
+// data vault
+use data_vault::RedisDataVault;
+// swappable encryption
+use data_vault::encryption::AesGcmSivEncryption;
+// swappable tokenizer
+use data_vault::tokenizer::Blake3Tokenizer;
+
+// credit card type
 use credit_card::CreditCard;
 
-let vault = RedisDataVault::<AesGcmSivEncryption, Blake3Tokenizer>::new().unwrap();
-
-let cc = CreditCard {
-    number: "4111111111111111".to_string(),
-    cardholder_name: "Graydon Hoare".to_string(),
-    expiration_month: "01".to_string(),
-    expiration_year: "2023".to_string(),
-    brand: None,
-    security_code: None
-};
-
-let token = vault.store_credit_card(&cc).await.unwrap();
-let credit_card = vault.retrieve_credit_card(&token.to_string()).await.unwrap();
-assert_eq!(credit_card.number, cc.number)
+fn main() {
+    let vault = RedisDataVault::<AesGcmSivEncryption, Blake3Tokenizer>::new().unwrap();
+    
+    let cc = CreditCard {
+        number: "4111111111111111".to_string(),
+        cardholder_name: "Graydon Hoare".to_string(),
+        expiration_month: "01".to_string(),
+        expiration_year: "2023".to_string(),
+        brand: None,
+        security_code: None
+    };
+    
+    let token = vault.store_credit_card(&cc).await.unwrap();
+    let credit_card = vault.retrieve_credit_card(&token.to_string()).await.unwrap();
+    assert_eq!(credit_card.number, cc.number)
+}
 ```
 
 # Current Features
@@ -57,7 +70,6 @@ assert_eq!(credit_card.number, cc.number)
 - Swappable Tokenization hasher
 
 # Future Features
-- Swappable Tokenization hasher
 - Postgres Database
 
 # Performance
--- src/lib.rs
+++ src/lib.rs
@@ -2,42 +2,77 @@
 //!
 //! ## Example
 //!
-//! ```rust,ignore
-//! # traits
-//! use crate::traits::DataVault;
-//! use crate::encryption::traits::Encryption;
-//!
-//! # data vault
-//! use crate::redis_data_vault::RedisDataVault;
-//! # swappable encryption
-//! use crate::encryption::AesGcmSivEncryption;
-//! # swappable tokenizer
-//! use crate::tokenizer::Blake3Tokenizer;
-//!
-//! # credit card type
-//! use credit_card::CreditCard;
-//!
-//! let vault = RedisDataVault::<AesGcmSivEncryption, Blake3Tokenizer>::new().unwrap();
-//!
-//! let cc = CreditCard {
-//! number: "4111111111111111".to_string(),
-//! cardholder_name: "Graydon Hoare".to_string(),
-//! expiration_month: "01".to_string(),
-//! expiration_year: "2023".to_string(),
-//! brand: None,
-//! security_code: None
-//! };
+//! ```toml
+//! ##x Cargo.toml
+//! [dependencies]
+//! data_vault = "^0.2"
 //! ```
 //!
-//! ## Features
+//! ```dotenv
+//! ## .env OR environment variables
+//! REDIS_URL=redis://:foobared@127.0.0.1/
+//! ## REDIS_POOL_MAX_SIZE=16
+//! ENCRYPTED_DATA_VAULT_KEY=000102030405060708090a0b0c0d0e0f
+//! ENCRYPTED_DATA_VAULT_IV=f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff
+//! ```
 //!
-//! * Store [Credit Cards](https://github.com/chmoder/credit_card)
-//! * Store `String`
-//! * Automatic Encryption and Decryption
-//! * Blake3 tokenization
-//! * Redis Server, URL connection configuration
-//! * Configurable from .env file or Environment Variables
-//! * Runs on stable Rust
+//! ```rust
+//! /// example.rs
+//!
+//! // traits
+//! use data_vault::DataVault;
+//! use data_vault::encryption::traits::Encryption;
+//!
+//! // data vault
+//! use data_vault::RedisDataVault;
+//! // swappable encryption
+//! use data_vault::encryption::AesGcmSivEncryption;
+//! // swappable tokenizer
+//! use data_vault::tokenizer::Blake3Tokenizer;
+//!
+//! // credit card type
+//! use credit_card::CreditCard;
+//!
+//! fn main() {
+//!     let vault = RedisDataVault::<AesGcmSivEncryption, Blake3Tokenizer>::new().unwrap();
+//!
+//!     let cc = CreditCard {
+//!         number: "4111111111111111".to_string(),
+//!         cardholder_name: "Graydon Hoare".to_string(),
+//!         expiration_month: "01".to_string(),
+//!         expiration_year: "2023".to_string(),
+//!         brand: None,
+//!         security_code: None
+//!     };
+//!
+//!     let token = vault.store_credit_card(&cc).await.unwrap();
+//!     let credit_card = vault.retrieve_credit_card(&token.to_string()).await.unwrap();
+//!     assert_eq!(credit_card.number, cc.number)
+//! }
+//! ```
+//!
+//! # Current Features
+//! - Store [Credit Cards](https://github.com/chmoder/credit_card)
+//! - Store `String`
+//! - Automatic Encryption and Decryption
+//! - Blake3 tokenization
+//! - Redis Server, URL connection configuration
+//! - Configurable from .env file or Environment Variables
+//! - Swappable Encryption
+//! - Swappable Tokenization hasher
+//!
+//! # Future Features
+//! - Swappable Tokenization hasher
+//! - Postgres Database
+//!
+//! # Performance
+//! This [example](https://github.com/chmoder/data_vault/blob/master/examples/benchmark.rs) output the following performance stats with an AMD Ryzen 9 3900X.
+//! Showing the possibility of tokenizing **~100,000** credit cards per second.
+//!
+//! tokenized and stored 100000 credit cards in 1.058474365s
+//! retrieved 100000 credit cards in 5.353857633s
+//! tokenized, stored, and retrieved 100000 credit cards in 6.412331998s
+//!
 
 mod traits;
 mod redis_data_vault;
