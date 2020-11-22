5822733e11f501c2f458099ab663264e5bccefab
chmoder <tom.bz2@gmail.com>
2020-11-21 23:07:53 -0600

    create new error type for trait to support multiple backend pool errors

--- src/traits.rs
+++ src/traits.rs
@@ -1,7 +1,23 @@
 use async_trait::async_trait;
 use credit_card::CreditCard;
-use deadpool_redis::PoolError;
+use deadpool_redis::PoolError as RedisPoolError;
+use deadpool_postgres::PoolError as PostgresPoolError;
 use std::error;
+
+
+#[derive(Debug)]
+pub enum PoolErrors {
+    RedisPoolError,
+    PostgresPoolError
+}
+
+impl From<RedisPoolError> for PoolErrors {
+    fn from(_: RedisPoolError) -> Self {PoolErrors::RedisPoolError}
+}
+
+impl From<PostgresPoolError> for PoolErrors {
+    fn from(_: PostgresPoolError) -> Self {PoolErrors::PostgresPoolError}
+}
 
 /// This is what a Data Vault can do
 /// It's fundamental purpose is to store and retrieve
@@ -10,8 +26,8 @@ use std::error;
 pub trait DataVault {
     fn new() -> Result<Self, Box<dyn error::Error>>
         where Self: std::marker::Sized;
-    async fn store(&self, token: &String, string: &String) -> Result<(), PoolError>;
-    async fn store_credit_card(&self, credit_card: &CreditCard) -> Result<String, PoolError>;
-    async fn retrieve(&self, token: &String) -> Result<String, PoolError>;
-    async fn retrieve_credit_card(&self, token: &String)  -> Result<CreditCard, PoolError>;
+    async fn store(&self, token: &String, string: &String) -> Result<(), PoolErrors>;
+    async fn store_credit_card(&self, credit_card: &CreditCard) -> Result<String, PoolErrors>;
+    async fn retrieve(&self, token: &String) -> Result<String, PoolErrors>;
+    async fn retrieve_credit_card(&self, token: &String)  -> Result<CreditCard, PoolErrors>;
 }
