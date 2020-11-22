78ecf06f1ef49169e7ada405975bea903c6079f8
chmoder <tom.bz2@gmail.com>
2020-11-21 23:06:30 -0600

    add datavault config

--- src/config.rs
+++ src/config.rs
@@ -14,6 +14,12 @@ pub struct DeadpoolRedisConfig {
     pub redis: deadpool_redis::Config,
 }
 
+#[derive(Debug, Deserialize, Default)]
+pub struct DeadpoolPostgresConfig {
+    #[serde(default)]
+    pub postgres: deadpool_postgres::Config,
+}
+
 /// Populates a configuration from .env file or Environment Variables
 /// for `encryption::Aes128CbcEncryption`.
 /// Possible Values:
@@ -42,5 +48,20 @@ impl DeadpoolRedisConfig {
         cfg.merge(environment)?;
         cfg.try_into()
     }
-
 }
+
+/// Populates a configuration from .env file or Environment Variables
+/// for `postgres_data_vault::DeadpoolPostgresConfig`.
+/// Possible Values:
+/// REDIS_URL=redis://:foobared@127.0.0.1/
+/// REDIS_POOL_MAX_SIZE=16
+impl DeadpoolPostgresConfig {
+    pub fn from_env() -> Result<Self, ::config_crate::ConfigError> {
+        dotenv().ok();
+        let mut cfg = ::config_crate::Config::new();
+        let environment = ::config_crate::Environment::new().separator(".");
+        cfg.merge(environment)?;
+        cfg.try_into()
+    }
+}
+
