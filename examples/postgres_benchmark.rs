e0d07c6fdd7d2bdea72205958b2800e525d8d114
chmoder <tom.bz2@gmail.com>
2021-06-25 12:31:57 -0500

    use threads to read credit cards

--- examples/postgres_benchmark.rs
+++ examples/postgres_benchmark.rs
@@ -1,7 +1,7 @@
 use log::{info};
 use tokio;
 use credit_card::CreditCard;
-use data_vault::{RedisDataVault, DataVault};
+use data_vault::{PostgresDataVault, DataVault};
 use std::vec;
 use std::time::{Instant};
 use std::sync::Arc;
@@ -13,7 +13,8 @@ async fn main() {
     env_logger::init();
 
     let mut token_futures = vec::Vec::new();
-    let redis_vault = RedisDataVault::<AesGcmSivEncryption, Blake3Tokenizer>::new().unwrap();
+    let mut result_futures = vec::Vec::new();
+    let redis_vault = PostgresDataVault::<AesGcmSivEncryption, Blake3Tokenizer>::new().unwrap();
     let vault = Arc::new(redis_vault);
     let cc = Arc::new(CreditCard {
         number: "4111111111111111".to_string(),
@@ -43,10 +44,16 @@ async fn main() {
     println!("tokenized and stored {} credit cards in {:?}", to_store, stored_time.duration_since(start_time));
 
     for token_result in results {
+        let vault = vault.clone();
         let token_result = token_result.unwrap();
         let token = token_result.unwrap();
-        let _credit_card = vault.retrieve_credit_card(&token).await.unwrap();
+        result_futures.push(
+            tokio::task::spawn(async move {
+                vault.retrieve_credit_card(&token).await
+            })
+        );
     }
+    futures::future::join_all(result_futures).await;
 
     let end_time = Instant::now();
     println!("retrieved {} credit cards in {:?}", to_store, end_time.duration_since(stored_time));
--- examples/redis_benchmark.rs
+++ examples/redis_benchmark.rs
@@ -13,6 +13,7 @@ async fn main() {
     env_logger::init();
 
     let mut token_futures = vec::Vec::new();
+    let mut result_futures = vec::Vec::new();
     let redis_vault = RedisDataVault::<AesGcmSivEncryption, Blake3Tokenizer>::new().unwrap();
     let vault = Arc::new(redis_vault);
     let cc = Arc::new(CreditCard {
@@ -43,10 +44,16 @@ async fn main() {
     println!("tokenized and stored {} credit cards in {:?}", to_store, stored_time.duration_since(start_time));
 
     for token_result in results {
+        let vault = vault.clone();
         let token_result = token_result.unwrap();
         let token = token_result.unwrap();
-        let _credit_card = vault.retrieve_credit_card(&token).await.unwrap();
+        result_futures.push(
+            tokio::task::spawn(async move {
+                vault.retrieve_credit_card(&token).await
+            })
+        );
     }
+    futures::future::join_all(result_futures).await;
 
     let end_time = Instant::now();
     println!("retrieved {} credit cards in {:?}", to_store, end_time.duration_since(stored_time));
